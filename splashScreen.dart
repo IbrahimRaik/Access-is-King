282bf2c79a5c37ebb4c2eacf8b3bc587e9cdbd2f
IbrahimRaik <im.momin.34@gmail.com>
2024-08-22 13:07:54 +0530

    Add files via upload

---
+++ splashScreen.dart
@@ -0,0 +1,33 @@
+import 'package:access_is_king/views/signUpScreen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                SignUpScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset(
+            'assets/images/crown.png'),
+      ),
+    );
+  }
+}
